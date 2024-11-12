c8b0351e7efc059ae92afe169cb5305421a3dcb6
Mykyta Trufanov <trufanov.n@outlook.com>
2024-11-12 21:29:07 +0100

    Application init

---
+++ src/main/java/com/trufanov/BankingBackendApiApplication.java
@@ -0,0 +1,11 @@
+package com.trufanov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingBackendApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BankingBackendApiApplication.class);
+    }
+}
