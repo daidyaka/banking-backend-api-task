a06edc21deda17bf7bcdf5da534d24d444ef5e2f
Mykyta Trufanov <trufanov.n@outlook.com>
2024-11-21 14:49:52 +0100

    fixing

--- src/main/java/com/trufanov/message/BalanceRefreshEventListener.java
+++ src/main/java/com/trufanov/message/BalanceRefreshEventListener.java
@@ -3,15 +3,16 @@ package com.trufanov.message;
 import com.trufanov.message.dto.BalanceRefreshEvent;
 import com.trufanov.service.AccountService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
 public class BalanceRefreshEventListener {
     private final AccountService accountService;
 
-    @EventListener
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleEvent(BalanceRefreshEvent event) {
         accountService.refreshBalanceCache(event.getAccountId());
     }
--- src/main/java/com/trufanov/service/TransactionService.java
+++ src/main/java/com/trufanov/service/TransactionService.java
@@ -1,10 +1,8 @@
 package com.trufanov.service;
 
 import com.trufanov.entity.Transaction;
-import jakarta.transaction.Transactional;
 
 public interface TransactionService {
 
-    @Transactional
     void makeTransaction(Transaction transaction);
 }
